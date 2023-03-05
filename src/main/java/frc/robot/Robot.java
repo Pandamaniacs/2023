c78a0884b27f3c3bb1b2b53f80644bb875df1ea6
Benjamin Rueter <brueter918@gmail.com>
2023-03-05 17:45:40 -0500

    added led code and class for controlling leds

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,12 +12,12 @@ import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonUtils;
 import org.photonvision.targeting.PhotonTrackedTarget;
 import edu.wpi.first.wpilibj.Timer;
+
+import java.util.Arrays;
 import java.util.List;
 import com.revrobotics.*;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.AddressableLED;
-import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
@@ -25,8 +25,8 @@ import edu.wpi.first.wpilibj.Solenoid;
 
 public class Robot extends TimedRobot {
   PhotonCamera camera = new PhotonCamera("Microsoft_LifeCam_HD-3000");
-  private XboxController driverController = new XboxController (0);
-  private XboxController shootController = new XboxController (1);
+  private XboxController driverController = new XboxController(0);
+  private XboxController shootController = new XboxController(1);
   private DifferentialDrive myRobot;
 
   private static final int leftLeadID = 1;
@@ -37,23 +37,38 @@ public class Robot extends TimedRobot {
   private static final int rightFollow2ID = 7;
   private static final int intakeID = 10;
 
-  private CANSparkMax leftLead;
+  private CANSparkMax leftLeader;
   private CANSparkMax leftFollow1;
   private CANSparkMax leftFollow2;
-  private CANSparkMax rightLead;
+  private CANSparkMax rightLeader;
   private CANSparkMax rightFollow1;
   private CANSparkMax rightFollow2;
   private CANSparkMax intakeMotor;
+
   private final Timer timer = new Timer();
 
-  AddressableLED indicatorLed;
-  AddressableLEDBuffer ledBuffer;
-  final int ledPort = 0;
-  final int ledLength = 20;
-  //RED = NO TARGET
-  //BLUE = NOT AIMING
-  //YELLOW = TARGET BUT NOT READY TO SHOOT
-  //GREEN = READY TO SHOOT
+  LED leftLed;
+  final int leftLedPort = 0;
+  final int statusLedLength = 12;
+  LED rightLed;
+  final int rightLedPort = 1;
+  LED directionLed;
+  final int directionLedPort = 2;
+  final int directionLedLength = 9;
+  /**
+   * ALWAYS ENSURE LEDS ARE MOUNTED WITH PWM CABLE ON THE TOP LEFT
+   */
+
+  int status = 0;
+  boolean high = false;
+  int[] search;
+  boolean flash = false;
+  /**
+   * 0: not searching
+   * 1: searching, no target
+   * 2: lining up
+   * 3: ready
+   */
 
   Compressor phCompressor = new Compressor(1, PneumaticsModuleType.REVPH);
   Solenoid phSolenoid = new Solenoid(PneumaticsModuleType.REVPH, 0);
@@ -70,9 +85,9 @@ public class Robot extends TimedRobot {
 
   double degrees = Units.radiansToDegrees(0);
 
-  final double CAMERA_HEIGHT_METERS = Units.inchesToMeters(24);//height of the camera from the ground
-  final double TARGET_HEIGHT_METERS = Units.feetToMeters(5);//height of target
-  final double GOAL_RANGE_METERS = Units.feetToMeters(3);//goal range
+  final double CAMERA_HEIGHT_METERS = Units.inchesToMeters(24);// height of the camera from the ground
+  final double TARGET_HEIGHT_METERS = Units.feetToMeters(5);// height of target
+  final double GOAL_RANGE_METERS = Units.feetToMeters(3);// goal range
   final double CAMERA_PITCH_RADIANS = Units.degreesToRadians(0);// Angle between horizontal and the camera.
 
   DigitalInput back = new DigitalInput(0);
@@ -91,59 +106,147 @@ public class Robot extends TimedRobot {
     camera.setDriverMode(false);
     camera.setPipelineIndex(0);
 
-    leftLead = new CANSparkMax(leftLeadID, MotorType.kBrushless);
+    initMotors();
+    initLEDS();
+
+    myRobot = new DifferentialDrive(leftLeader, rightLeader);
+    phCompressor.enableDigital();
+
+    addPeriodic(() -> {
+      updateLedPattern();
+    }, 0.01, 0.005);
+  }
+
+  int first = 0;
+
+  /**
+   * 0: not searching - blank
+   * 1: searching, no target - searching
+   * 2: lining up - flash
+   * 3: ready - red/white, low/high
+   */
+  
+  /**
+   * 3x4 led map
+   * 0 1 2 3
+   * 7 6 5 4
+   * 8 9 10 11
+   */
+  /**
+   * 3x3 led map
+   * 0 1 2
+   * 5 4 3
+   * 6 7 8
+   */
+  
+  int[] statusMap = {0,  1,  2,  3,
+                     7,  6,  5,  4,
+                     8,  9,  10, 11};
+  int[] searchMap = {0, 1, 2, 3, 4, 11, 10, 9, 8, 7};
+  int[] searchOutput = new int[searchMap.length];
+  int[] dirMap = {0,  1,  2,
+                  5,  4,  3,
+                  6,  7,  8};
+  public void updateLedPattern() {
+    switch(status) {
+      case 0:
+        leftLed.clear();
+        rightLed.clear();
+        break;
+
+      case 1:
+      first = search[0];
+        for(int i = 0 ; i < search.length - 1 ; i++) {
+          search[i] = search[i + 1];
+        }
+        Arrays.fill(searchOutput, 0);
+        for(int i = 0 ; i < searchMap.length ; i++) {
+          for(int j = 0 ; j < statusMap.length ; i++) {
+            if(statusMap[j] == searchMap[i]) {
+              searchOutput[statusMap[j]] = search[i];
+              break;
+            }
+          }
+        }
+        leftLed.setPatternSingleColor(searchOutput);
+        search[search.length - 1] = first;
+        break;
+
+      case 2:
+        flash = !flash;
+        if(flash) {
+          leftLed.setSolid(255, 0, 0);
+          rightLed.setSolid(255, 255, 255);
+        } else {
+          leftLed.setSolid(255, 255, 255);
+          rightLed.setSolid(255, 0, 0);
+        }
+        break;
+
+      case 3:
+        if(high) {
+          leftLed.setSolid(255, 255, 255);
+          rightLed.setSolid(255, 255, 255);
+        } else {
+          leftLed.setSolid(255, 0, 0);
+          rightLed.setSolid(255, 0, 0);
+        }
+        break;
+    }
+  }
+
+  public void initMotors() {
+    leftLeader = new CANSparkMax(leftLeadID, MotorType.kBrushless);
     leftFollow1 = new CANSparkMax(leftFollow1ID, MotorType.kBrushless);
-    rightLead = new CANSparkMax(rightLeadID, MotorType.kBrushless);
+    rightLeader = new CANSparkMax(rightLeadID, MotorType.kBrushless);
     rightFollow1 = new CANSparkMax(rightFollow1ID, MotorType.kBrushless);
     leftFollow2 = new CANSparkMax(leftFollow2ID, MotorType.kBrushless);
     rightFollow2 = new CANSparkMax(rightFollow2ID, MotorType.kBrushless);
     intakeMotor = new CANSparkMax(intakeID, MotorType.kBrushless);
 
-    leftLead.restoreFactoryDefaults();
+    leftLeader.restoreFactoryDefaults();
     leftFollow1.restoreFactoryDefaults();
     leftFollow2.restoreFactoryDefaults();
-    rightLead.restoreFactoryDefaults();
+    rightLeader.restoreFactoryDefaults();
     rightFollow1.restoreFactoryDefaults();
     rightFollow2.restoreFactoryDefaults();
     intakeMotor.restoreFactoryDefaults();
 
     int driveCurrentLimit = 60;
     int intakeCurrentLimit = 30;
-    leftLead.setSmartCurrentLimit(driveCurrentLimit);
+    leftLeader.setSmartCurrentLimit(driveCurrentLimit);
     leftFollow1.setSmartCurrentLimit(driveCurrentLimit);
     leftFollow2.setSmartCurrentLimit(driveCurrentLimit);
-    rightLead.setSmartCurrentLimit(driveCurrentLimit);
+    rightLeader.setSmartCurrentLimit(driveCurrentLimit);
     rightFollow1.setSmartCurrentLimit(driveCurrentLimit);
     rightFollow2.setSmartCurrentLimit(driveCurrentLimit);
     intakeMotor.setSmartCurrentLimit(intakeCurrentLimit);
 
-    leftFollow1.follow(leftLead);
-    leftFollow2.follow(leftLead);
-    rightFollow1.follow(rightLead);
+    leftFollow1.follow(leftLeader);
+    leftFollow2.follow(leftLeader);
+    rightFollow1.follow(rightLeader);
     rightFollow2.follow(rightFollow1);
 
-    leftLead.burnFlash();
+    leftLeader.burnFlash();
     leftFollow1.burnFlash();
     leftFollow2.burnFlash();
-    rightLead.burnFlash();
+    rightLeader.burnFlash();
     rightFollow1.burnFlash();
     rightFollow2.burnFlash();
     intakeMotor.burnFlash();
+  }
 
-    myRobot = new DifferentialDrive(leftLead, rightLead);
-    phCompressor.enableDigital();
+  public void initLEDS() {
+    leftLed = new LED(leftLedPort, statusLedLength);
+    rightLed = new LED(rightLedPort, statusLedLength);
+    directionLed = new LED(directionLedPort, directionLedLength);
+    
+    search = new int[10];
 
-    indicatorLed = new AddressableLED(ledPort);
-    ledBuffer = new AddressableLEDBuffer(ledLength);
-    indicatorLed.setLength(ledBuffer.getLength());
-    indicatorLed.setData(ledBuffer);
-    indicatorLed.start();
-
-    for(int i = 0 ; i < ledBuffer.getLength() ; i++) {
-      ledBuffer.setRGB(i, 0, 0, 255);
+    for (int i = 0 ; i < search.length ; i++) {
+      search[i] = 255/search.length*i;
     }
-    indicatorLed.setData(ledBuffer);
- }
+  }
 
   @Override
   public void autonomousInit() {
@@ -151,35 +254,35 @@ public class Robot extends TimedRobot {
     timer.start();
     setLed(0, 0, 255);
   }
-    
+
   @Override
   public void autonomousPeriodic() {
     double speed = 0.75;
-   if (back.get()) {
-    if (timer.get() < 2.0) {
-      leftLead.set (speed);
-      rightLead.set(speed * -1);
-    }} else if (shoot.get()) {
+    if (back.get()) {
+      if (timer.get() < 2.0) {
+        leftLeader.set(speed);
+        rightLeader.set(speed * -1);
+      }
+    } else if (shoot.get()) {
       if (timer.get() < 5.0) {
         phSolenoid.set(true);
         Timer.delay(.50);
         phSolenoid.set(false);
-        leftLead.set(speed * -1);
-        rightLead.set(speed);
+        leftLeader.set(speed * -1);
+        rightLeader.set(speed);
       }
-    } else if (intake.get()){
+    } else if (intake.get()) {
       if (timer.get() < 4.0) {
         phSolenoid.set(true);
         Timer.delay(.50);
         phSolenoid.set(false);
-        leftLead.set(speed * -1);
-        rightLead.set(speed);
-        //intake code added here :)
+        leftLeader.set(speed * -1);
+        rightLeader.set(speed);
+        // intake code added here :)
       }
+    } else {
+      myRobot.stopMotor();
     }
-    else {
-        myRobot.stopMotor();
-      } 
   }
 
   double forward = 0;
@@ -195,31 +298,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
-    p_dist = SmartDashboard.getNumber("kP_dist", 0);
-    d_dist = SmartDashboard.getNumber("kD_dist", 0);
-    p_angle = SmartDashboard.getNumber("kP_angle", 0);
-    d_angle = SmartDashboard.getNumber("kD_angle", 0);
-    
-    if(p_dist != kP_dist) { 
-      kP_dist = p_dist;
-      SmartDashboard.putNumber("kP_dist", kP_dist);
-      PID_dist.setP(kP_dist);
-    }
-    if(d_dist != kD_dist) { 
-      kD_dist = d_dist;
-      SmartDashboard.putNumber("kD_dist", kP_dist);
-      PID_dist.setD(kD_dist);
-    }
-    if(p_angle != kP_angle) { 
-      kP_angle = p_angle;
-      SmartDashboard.putNumber("kP_angle", kP_dist);
-      PID_angle.setP(kP_angle);
-    }
-    if(d_angle != kD_angle) { 
-      kD_angle = d_angle;
-      SmartDashboard.putNumber("kD_angle", kP_dist);
-      PID_angle.setD(kD_angle);
-    }
+    handleDash();
 
     double leftYstick = (driverController.getLeftY());
     double rightXstick = (driverController.getRightX());
@@ -231,31 +310,23 @@ public class Robot extends TimedRobot {
       Timer.delay(1.0);
     } else if (driverController.getBButtonReleased()) {
       phSolenoid.set(false);
-    } 
+    }
 
-    if (driverController.getXButtonPressed()){
+    if (driverController.getXButtonPressed()) {
       phCompressor.disable();
     }
 
-    if(driverController.getAButton()) {
+    if (driverController.getAButton()) {
       var result = camera.getLatestResult();
 
-      if(result.hasTargets()) {
+      if (result.hasTargets()) {
         List<PhotonTrackedTarget> targets = result.getTargets();
         PhotonTrackedTarget target = targets.get(0);
-        /*SmartDashboard.putNumber("target ID", target.getFiducialId());
-        SmartDashboard.putNumber("X (forward)", target.getBestCameraToTarget().getTranslation().getX());
-        SmartDashboard.putNumber("Y (left)", target.getBestCameraToTarget().getTranslation().getY());
-        SmartDashboard.putNumber("Z (up)", target.getBestCameraToTarget().getTranslation().getZ());
-        SmartDashboard.putNumber("X ROTATION (roll)", Units.radiansToDegrees(target.getBestCameraToTarget().getRotation().getX()));
-        SmartDashboard.putNumber("Y ROTATION (pitch)", Units.radiansToDegrees(target.getBestCameraToTarget().getRotation().getY()));
-        SmartDashboard.putNumber("Z ROTATION (yaw)", Units.radiansToDegrees(target.getBestCameraToTarget().getRotation().getZ()));*/
-        double range =
-        PhotonUtils.calculateDistanceToTargetMeters(
-                CAMERA_HEIGHT_METERS,
-                TARGET_HEIGHT_METERS,
-                CAMERA_PITCH_RADIANS,
-                Units.degreesToRadians(result.getBestTarget().getPitch()));
+        double range = PhotonUtils.calculateDistanceToTargetMeters(
+            CAMERA_HEIGHT_METERS,
+            TARGET_HEIGHT_METERS,
+            CAMERA_PITCH_RADIANS,
+            Units.degreesToRadians(result.getBestTarget().getPitch()));
 
         forward = -PID_dist.calculate(range, 30);
         steer = -PID_angle.calculate(result.getBestTarget().getYaw(), 0);
@@ -263,34 +334,33 @@ public class Robot extends TimedRobot {
         distAtSet = PID_dist.atSetpoint();
         angleAtSet = PID_angle.atSetpoint();
         ready = distAtSet && angleAtSet;
-        SmartDashboard.putBoolean("has target", true);//we see a target!
-        SmartDashboard.putBoolean("distance good", distAtSet);//are we at a good distance?
-        SmartDashboard.putBoolean("angle good", angleAtSet);//are we at a good angle?
-        SmartDashboard.putBoolean("ready", ready);//are we at a good distance and angle?
+        SmartDashboard.putBoolean("has target", true);// we see a target!
+        SmartDashboard.putBoolean("distance good", distAtSet);// are we at a good distance?
+        SmartDashboard.putBoolean("angle good", angleAtSet);// are we at a good angle?
+        SmartDashboard.putBoolean("ready", ready);// are we at a good distance and angle?
 
-        if((PID_dist.atSetpoint() & PID_angle.atSetpoint())) {//yes!
-          driverController.setRumble(RumbleType.kLeftRumble, .1);//let the drivers know with some rumble!
+        if ((PID_dist.atSetpoint() & PID_angle.atSetpoint())) {// yes!
+          driverController.setRumble(RumbleType.kLeftRumble, .1);// let the drivers know with some rumble!
           shootController.setRumble(RumbleType.kLeftRumble, .1);
-          setLed(0, 255, 0);//ready!
-        } else {//no
-          driverController.setRumble(RumbleType.kLeftRumble, 0);//rumble needs to be explicitly set to 0
-          shootController.setRumble(RumbleType.kLeftRumble, 0);
-          setLed(255, 255, 0);//we have a target, but were not in range
         }
-      } else {//no target, reset dashboard to let drivers know and disable steering
+        else {// no
+          driverController.setRumble(RumbleType.kLeftRumble, 0);// rumble needs to be explicitly set to 0
+          shootController.setRumble(RumbleType.kLeftRumble, 0);
+        }
+      }
+      else {// no target, reset dashboard to let drivers know and disable steering
         resetDash();
         forward = 0;
         steer = 0;
-        setLed(255, 0, 0);
       }
-    } else {
+    }
+    else {
       resetDash();
-      setLed(0, 0, 255);
     }
 
-    if(ready) {
-      //if shoot button is pressed, shoot()
-    } //else if manual override buttons are held, shoot()
+    if (ready) {
+      // if shoot button is pressed, shoot()
+    } // else if manual override buttons are held, shoot()
 
     if (shootController.getLeftBumperPressed()) {
       phSolenoid2.set(true);
@@ -316,13 +386,51 @@ public class Robot extends TimedRobot {
   }
 
   public void setLed(int r, int g, int b) {
-    for(int i = 0 ; i < ledBuffer.getLength() ; i++) {
-      ledBuffer.setRGB(i, 255, 0, 0);
-    }
-    indicatorLed.setData(ledBuffer);
+
   }
 
-  public void shoot() {
+  public void handleDash() {
 
+    p_dist = SmartDashboard.getNumber("kP_dist", 0);
+    d_dist = SmartDashboard.getNumber("kD_dist", 0);
+    p_angle = SmartDashboard.getNumber("kP_angle", 0);
+    d_angle = SmartDashboard.getNumber("kD_angle", 0);
+
+    if (p_dist != kP_dist) {
+      kP_dist = p_dist;
+      SmartDashboard.putNumber("kP_dist", kP_dist);
+      PID_dist.setP(kP_dist);
+    }
+    if (d_dist != kD_dist) {
+      kD_dist = d_dist;
+      SmartDashboard.putNumber("kD_dist", kP_dist);
+      PID_dist.setD(kD_dist);
+    }
+    if (p_angle != kP_angle) {
+      kP_angle = p_angle;
+      SmartDashboard.putNumber("kP_angle", kP_dist);
+      PID_angle.setP(kP_angle);
+    }
+    if (d_angle != kD_angle) {
+      kD_angle = d_angle;
+      SmartDashboard.putNumber("kD_angle", kP_dist);
+      PID_angle.setD(kD_angle);
+    }
   }
 }
+
+/**
+  * SmartDashboard.putNumber("target ID", target.getFiducialId());
+  * SmartDashboard.putNumber("X (forward)",
+  * target.getBestCameraToTarget().getTranslation().getX());
+  * SmartDashboard.putNumber("Y (left)",
+  * target.getBestCameraToTarget().getTranslation().getY());
+  * SmartDashboard.putNumber("Z (up)",
+  * target.getBestCameraToTarget().getTranslation().getZ());
+  * SmartDashboard.putNumber("X ROTATION (roll)",
+  * Units.radiansToDegrees(target.getBestCameraToTarget().getRotation().getX()));
+  * SmartDashboard.putNumber("Y ROTATION (pitch)",
+  * Units.radiansToDegrees(target.getBestCameraToTarget().getRotation().getY()));
+  * SmartDashboard.putNumber("Z ROTATION (yaw)",
+  * Units.radiansToDegrees(target.getBestCameraToTarget().getRotation().getZ()));
+  */
